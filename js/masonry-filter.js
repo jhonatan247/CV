edfb3a52d09faf82b55a95703fcd1f286900db34
Jhonatan Torres <yon.t.f@hotmail.com>
2019-01-02 09:28:29 -0500

    format

--- js/masonry-filter.js
+++ js/masonry-filter.js
@@ -1,31 +1,31 @@
-(function($){
+(function ($) {
   'use strict';
-  
-  $.fn.multipleFilterMasonry = function(options){
-    var cache=[];
+
+  $.fn.multipleFilterMasonry = function (options) {
+    var cache = [];
     var filters = [];
 
-    if(options.selectorType === 'list') {
-      $(options.filtersGroupSelector).children().each(function() {
+    if (options.selectorType === 'list') {
+      $(options.filtersGroupSelector).children().each(function () {
         filters.push($(this).data('filter'));
       });
     }
 
     //the main job of the function is to cache the item,because we are going to filter the items later
-    var init = function($container){
-      $container.find(options.itemSelector).each(function(){
+    var init = function ($container) {
+      $container.find(options.itemSelector).each(function () {
         cache.push($(this));
       });
       $container.masonry(options);
     };
 
     //filter items in cache
-    var filterItems = function(selector){
-      var result=[];
-      $(cache).each(function(item){
-        $(selector).each(function(index,sel) {
-          if(cache[item].is(sel)){
-            if($.inArray(cache[item], result) === -1) result.push(cache[item]);
+    var filterItems = function (selector) {
+      var result = [];
+      $(cache).each(function (item) {
+        $(selector).each(function (index, sel) {
+          if (cache[item].is(sel)) {
+            if ($.inArray(cache[item], result) === -1) result.push(cache[item]);
           }
         });
       });
@@ -33,9 +33,9 @@
     };
 
     //reload masonry
-    var reload = function($container,items){
+    var reload = function ($container, items) {
       $container.empty();
-      $(items).each(function(){
+      $(items).each(function () {
         $($container).append($(this));
       });
       $container.masonry('reloadItems');
@@ -43,53 +43,53 @@
     };
 
     // Hash filter
-    var hashFilter = function($container) {
+    var hashFilter = function ($container) {
       var hash = window.location.hash.replace("#", "");
-      if($.inArray(hash, filters) !== -1) {
+      if ($.inArray(hash, filters) !== -1) {
         reload($container, $('.' + hash));
       }
     }
 
-    var proc = function($container){
-      $(options.filtersGroupSelector).find('input[type=checkbox]').each(function(){
-        $(this).change(function(){
+    var proc = function ($container) {
+      $(options.filtersGroupSelector).find('input[type=checkbox]').each(function () {
+        $(this).change(function () {
           var selector = [];
-          $(options.filtersGroupSelector).find('input[type=checkbox]').each( function() {
-            if ( $(this).is(':checked') ) {
-              selector.push( '.' + $(this).val() );
+          $(options.filtersGroupSelector).find('input[type=checkbox]').each(function () {
+            if ($(this).is(':checked')) {
+              selector.push('.' + $(this).val());
             }
           });
           var items = cache;
           if (selector.length > 0) {
             items = filterItems(selector);
           }
-          reload($container,items);
+          reload($container, items);
         });
       });
     };
 
-    var procUL = function($container){
-      $(options.filtersGroupSelector).children().each(function(){
-        $(this).click(function(){
+    var procUL = function ($container) {
+      $(options.filtersGroupSelector).children().each(function () {
+        $(this).click(function () {
           $(options.filtersGroupSelector).children().removeClass('selected');
           window.location.hash = $(this).data('filter');
           var selector = [];
-          selector.push( '.' + $(this).data('filter') );
+          selector.push('.' + $(this).data('filter'));
           $(this).addClass('selected');
           var items = cache;
           if (selector.length > 0) {
             items = filterItems(selector);
           }
-          reload($container,items);
+          reload($container, items);
         });
       });
 
       hashFilter($container);
       $(options.filtersGroupSelector).children().removeClass('selected');
-      $('.filters li[data-filter='+window.location.hash.replace("#", "")+']').addClass('selected');
+      $('.filters li[data-filter=' + window.location.hash.replace("#", "") + ']').addClass('selected');
     };
 
-    return this.each(function() {
+    return this.each(function () {
       var $$ = $(this);
       init($$);
       options.selectorType === 'list' ? procUL($$) : proc($$);
